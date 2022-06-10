f27759d4b7fb2452deacfefd7b8a3f3e70ecfa64
Saul Corona <saulcoronascs@gmail.com>
2022-06-09 19:51:01 -0500

    test: Add unit tests for CurrencyFormatter

---
+++ Bankey/BankeyUnitTests/CurrencyFormatterTests.swift
@@ -0,0 +1,37 @@
+//
+//  CurrencyFormatterTests.swift
+//  BankeyUnitTests
+//
+//  Created by saul corona on 09/06/22.
+//
+
+import Foundation
+import XCTest
+
+@testable import Bankey // Import all proyect
+
+class Test: XCTestCase {
+    
+    var formatter: CurrencyFormatter!
+    
+    override func setUp() {
+        super.setUp()
+        formatter = CurrencyFormatter()
+    }
+    
+    func testBreakIntoDollarsAndCents() throws {
+        let result = formatter.breakIntoDollarsAndCents(929466.23)
+        XCTAssertEqual(result.0, "929,466")
+        XCTAssertEqual(result.1, "23")
+    }
+    
+    func testDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(100)
+        XCTAssertEqual(result, "$100.00")
+    }
+    
+    func testZeroDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(0.00)
+        XCTAssertEqual(result, "$0.00")
+    }
+}
